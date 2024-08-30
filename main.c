4c97cc8995ab63c09d137b0531c19d00e60c7fa7
ValentinMENDIss <valiksuper58@gmail.com>
2024-08-30 15:25:19 +0200

    Updated the structure of the main.c code (+deleted binary)

--- main.c
+++ main.c
@@ -1,35 +1,25 @@
 #include <stdio.h>
 #include <time.h>
 
-void delay(int number_of_milli_seconds)
-{
-int seconds = 1000 * number_of_milli_seconds;
-
-clock_t start_time = clock();
-
-
-while(clock() < start_time + seconds) {
-  ;
+void delay(int number_of_milli_seconds) {
+        int seconds = 1000 * number_of_milli_seconds;
+        clock_t start_time = clock();
+        while(clock() < start_time + seconds) {
+                ;
+        }
 }
-
-}
-
 int main() {
+        int i;
+        int timer;
 
-  int i;
+        printf("\nTimer Application\n");
+        printf("\nHow long should timer be (in seconds)?: ");
+        scanf("%d", &timer);
 
-  int timer;
+        for(i = 0; i < timer; i++) {
+                delay(1000);
+                printf("%d seconds have passed\n", i + 1);
+        }
 
-
-  printf("\nTimer Application\n");
-  printf("\nHow long should timer be (in seconds)?: ");
-  
-  scanf("%d", &timer);
-
-  for(i = 0; i < timer; i++) {
-    delay(1000);
-    printf("%d seconds have passed\n", i + 1);
-  }
-
-  return 0;
+        return 0;
 }
